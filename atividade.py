ed41159e25065fa0456c3dd6eec7d00f2dc527bb
Ana Beatriz <anabsenaf@gmail.com>
2023-03-05 22:02:22 -0300

    Atividade 01

---
+++ atividade.py
@@ -0,0 +1,10 @@
+n = ("nome: ")
+sn = ("sobrenome: ")
+id =int(input('idade: '))
+peso=float(input("peso: "))
+altura=float(input("altura: "))
+if id>=18:
+  print(f' {id}anos, maior de idade...')
+else:
+    print(f'{id}anos, menor de idade...')
+print(f'NOME: {n} \n SOBRENOME: {sn} \n IDADE: {id} \n PESO: {peso} \n ALTURA: {altura} cm')
