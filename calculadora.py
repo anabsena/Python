18f1b4511e78fa19a11ada294ac604fbc3723396
anabsena <anabsenaf@gmail.com>
2023-03-14 10:22:30 -0300

    Atividade 03

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,11 @@
 numero1 = float(input("Digite o primeiro numero: "))
 operador = input("Digite a operação: ")
 numero2 = float(input("Digite o segundo numero: "))
-try:
-    while True:
+while True:
+    try:
+        numero1 = float(input("Digite o primeiro número: "))
+        operador = input("Digite a operação (+, -, *, /): ")
+        numero2 = float(input("Digite o segundo número: "))
         if operador == "+":
             resultado = numero1 + numero2
         elif operador == "-":
@@ -12,10 +15,11 @@ try:
         elif operador == "/":
             resultado = numero1 / numero2
         else:
-            print("Operador invalido." + operador)
-        print("f{numero1} {operador} {numero2} = {resultado}")
-        sair = input("Deseja sair: ").lower.startswith('s')
+            print("Operador inválido:", operador)
+            continue
+        print(f"{numero1}, {operador}, {numero2}, = {resultado}")
+        sair = input("Deseja sair (s/n)? ").lower().startswith('s')
         if sair:
             break
-except:
-    print("Numero invalido.")
+    except ValueError:
+        print("Entrada inválida, tente novamente.")
