6993eb302ef8403bb0a22878a87acd1822613687
anabsena <anabsenaf@gmail.com>
2023-03-13 21:06:08 -0300

    atividade 03

---
+++ tabuada.py
@@ -0,0 +1,6 @@
+
+tabuada = int(input("Qual a tabuada você deseja? "))
+quantidade = 1
+while quantidade <= 10:
+    print(f"{tabuada} X {quantidade} = {quantidade * tabuada}")
+    quantidade += 1
---
+++ while02.py
@@ -0,0 +1,9 @@
+contador = 0
+while contador <= 100:
+    contador = contador + 1
+
+    if contador == 50:
+        print ("não vou exibir o numero 50")
+        continue
+        print("Acabou")
+    print(contador)
