07ca30c60d235724de3b74c875ad86268bd97db5
Ana Beatriz <anabsenaf@gmail.com>
2023-04-11 11:06:07 -0300

    Atividade 10

--- mediaNumeros.py
+++ mediaNumeros.py
@@ -1,1 +1,8 @@
-print('até dia 17 eu entrego');
+def calcular_media(lista_numeros):
+    media = sum(lista_numeros) / len(lista_numeros)
+    return media
+
+numeros = [10, 20, 30, 40, 50]
+media_calculada = calcular_media(numeros)
+
+print(f"A média dos números {numeros} é {media_calculada:.2f}")
