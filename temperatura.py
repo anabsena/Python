324f67014626851db51c07b6af6c12fc8bb3c588
Ana Beatriz <anabsenaf@gmail.com>
2023-04-11 10:35:21 -0300

    atividade 08

--- temperatura.py
+++ temperatura.py
@@ -1,1 +1,8 @@
-print('até dia 17 eu entrego')
+# OBS: a temperatura em fahrenheit estava com formula errada.
+temp = float(input("Digite uma temperatura em graus Celsius: "))
+
+def temperatura(temp):
+    Far = (9*temp/5) + 32
+    print(f"A temperatura em graus Fahrenheit é de {Far}F°")
+
+temperatura(temp)
