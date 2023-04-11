8ec2f197dc107a9b350427e35dcde50749776a79
Ana Beatriz <anabsenaf@gmail.com>
2023-04-11 11:02:34 -0300

    atividade 09

--- velocidade.py
+++ velocidade.py
@@ -1,1 +1,15 @@
-print('até dia 17 eu entrego');
+velocidade_media = float(input("Digite a velocidade média durante a viagem: "))
+tempo_gasto = float(input("Digite o tempo gasto durante a viagem: "))
+
+def distancia(velocidade_media, tempo_gasto):
+    return velocidade_media * tempo_gasto
+
+def litros(distancia):
+    return distancia / 12
+
+def resultados(velocidade_media, tempo_gasto):
+    dist = distancia(velocidade_media, tempo_gasto)
+    litros_usados = litros(dist)
+    print(f"A velocidade média foi: {velocidade_media} km/h, o tempo gasto foi: {tempo_gasto} horas, a distância percorrida foi: {dist} km e foram gastos {litros_usados:.2f} litros de combustível.")
+
+resultados(velocidade_media, tempo_gasto)
